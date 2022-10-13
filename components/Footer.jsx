3426932786a9ad09c26295d10dfbecdc8d9c7111
gaurav-jangam <gauravjangam444@gmail.com>
2022-10-13 21:41:40 +0530

    Added footer and resolve error

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,14 +4,14 @@ import { FooterPosts } from '../sections/index';
 
 const Footer = () => {
   return (
-      <footer class=" text-white bg-white">
-        <div class="p-6">
+      <footer className=" text-white bg-white">
+        <div className="p-6">
             <FooterPosts />
         </div>
 
-        <div class=" text-lg text-black font-semibold border-b pb-4 text-center p-4 bg-slate-100">
+        <div className=" text-lg text-black font-semibold border-b pb-4 text-center p-4 bg-slate-100">
         Copyright © {new Date().getFullYear()}
-            <a class="text-xl text-black mb-8 font-semibold border-b pb-4" href="https://gauravjangam.dev/" target="_blank"> Gaurav Jangam</a>
+            <a className="text-xl text-black mb-8 font-semibold border-b pb-4" href="https://gauravjangam.dev/" target="_blank"> Gaurav Jangam</a>
         </div>
     </footer>
   )
--- components/index.js
+++ components/index.js
@@ -11,3 +11,4 @@ export { default as FeaturedPostCard } from './FeaturedPostCard';
 export { default as FooterPostCard } from './FooterPostCard';
 export { default as Loader } from './Loader';
 export { default as Footer } from './Footer';
+export { default as AdjacentPostCard } from './AdjacentPostCard';
